793f71d40f92e8ab0ad1d8e217fd722467687eee
SabineWren <sabinewrensc@gmail.com>
2019-09-16 02:50:01 -0700

    Refactored test code; orbital period seems slightly off

--- tests/2body.mjs
+++ tests/2body.mjs
@@ -14,66 +14,78 @@
 import { RK4MutV, MidpointEulerMutV } from "../scripts/integrate.mjs";
 import { ComputePositionAfterTime } from "../scripts/nBody.mjs";
 import * as M3 from "../scripts/matrices3D.mjs";
+const G = 6.6743015 * Math.pow(10,-11);
 
-const G      = 6.6743015 * Math.pow(10,-11);
-const MASS_1 = 1.9885    * Math.pow(10, 30);
-const SEMI_MAJOR_AXIS = 149_598_023_000.0;
-//user inputs
-const mass2        = 5.97237 * Math.pow(10, 24);
-const eccentricity = 0.0167086;
-//https://en.wikipedia.org/wiki/Apsis
-const apoapsis  = SEMI_MAJOR_AXIS * (1 + eccentricity);
-const periapsis = SEMI_MAJOR_AXIS * (1 - eccentricity);
-
-const secToDay = seconds => seconds / 3600.0 / 24.0;
-
-//https://en.wikipedia.org/wiki/Gravitational_two-body_problem
-//T^2 G(m1 + m2) = 4 Pi^2 a^3
-const expectYearInSeconds = function() {
-	const aCubed = Math.pow(SEMI_MAJOR_AXIS, 3);
-	const denom = G * (mass2 + MASS_1);
-	return 2.0 * Math.PI * Math.sqrt(aCubed / denom);
-}();
-//why is the computed year different from the sidereal year on charts?
-//365.256363004 * 3600.0 * 24.0
-
-//https://en.wikipedia.org/wiki/Orbital_speed
-const SPEED_AT_APOAPSIS = Math.sqrt(G * MASS_1 * (1.0 - eccentricity) / apoapsis);
-//https://en.wikipedia.org/wiki/Barycenter
-const centreOfMassRatio = -1.0 * mass2 / (MASS_1 + mass2);
-
-{
-	let r2 = [apoapsis, 0.0, 0.0];
-	let v2 = [0.0, -1.0, 0.0].ScaleMut(SPEED_AT_APOAPSIS);
-	let r1 = r2.Scale(centreOfMassRatio);
-	let v1 = v2.Scale(centreOfMassRatio);
-	console.log(r2);
-
-	const timeStep = 1.0;
+const test2Body = function(params, dt, integrator) {
+	let r2 = [params.apoapsis, 0.0, 0.0];
+	let v2 = [0.0, -1.0, 0.0].ScaleMut(params.vApoapsis);
+	let r1 = r2.Scale(params.comRatio);
+	let v1 = v2.Scale(params.comRatio);
 	const start = Date.now();
+	
 	[r2, v2, r1, v1] = ComputePositionAfterTime(
-		MASS_1, mass2,
-		r1, r2, v1, v2, MidpointEulerMutV, expectYearInSeconds, timeStep
+		params.m1, params.m2,
+		r1, r2, v1, v2, integrator, params.orbitalPeriod, dt
 	);
+	
 	const delta = Date.now() - start;
-	console.log("MidEu: " + delta);
-	console.log(r2);
-}
+	return [r2, delta];
+};
 
-{
-	let r2 = [apoapsis, 0.0, 0.0];
-	let v2 = [0.0, -1.0, 0.0].ScaleMut(SPEED_AT_APOAPSIS);
-	let r1 = r2.Scale(centreOfMassRatio);
-	let v1 = v2.Scale(centreOfMassRatio);
+const createParams = function(eccentricity, mass1, mass2, semiMajorAxis) {
+	//https://en.wikipedia.org/wiki/Apsis
+	const apoapsis  = semiMajorAxis * (1.0 + eccentricity);
+	//const periapsis = semiMajorAxis * (1.0 - eccentricity);
 
-	const timeStep = 8.0;
-	const rk4Start = Date.now();
-	[r2, v2, r1, v1] = ComputePositionAfterTime(
-		MASS_1, mass2,
-		r1, r2, v1, v2, RK4MutV, expectYearInSeconds, timeStep
-	);
-	const rkDelta = Date.now() - rk4Start;
-	console.log("  RK4: " + rkDelta);
-	console.log(r2);
-}
+	//https://en.wikipedia.org/wiki/Barycenter
+	const centreOfMassRatio = -1.0 * mass2 / (mass1 + mass2);
+
+	//https://en.wikipedia.org/wiki/Orbital_speed
+	const vApoapsis = Math.sqrt(G * mass1 * (1.0 - eccentricity) / apoapsis);
+	
+	//https://en.wikipedia.org/wiki/Gravitational_two-body_problem
+	//T^2 G(m1 + m2) = 4 Pi^2 a^3
+	const aCubed = Math.pow(semiMajorAxis, 3);
+	const denom = G * (mass1 + mass2);
+	const orbitalPeriod = 2.0 * Math.PI * Math.sqrt(aCubed / denom);
+	
+	console.log(orbitalPeriod / (3600.0 * 24.0));
+	return {
+		apoapsis: apoapsis,
+		comRatio: centreOfMassRatio,
+		m1: mass1,
+		m2: mass2,
+		orbitalPeriod: orbitalPeriod,
+		//orbitalPeriod: 365.25335 * 24.0 * 3600.0,
+		//orbitalPeriod: 365.256363004 * 24.0 * 3600.0,
+		semiMajorAxis: semiMajorAxis,
+		vApoapsis: vApoapsis,
+	};
+};
+
+//const secToDay = seconds => seconds / 3600.0 / 24.0;
+const MASS_SUN   = 1.9885    * Math.pow(10, 30);
+const MASS_EARTH = 5.97237   * Math.pow(10, 24);
+const EARTH_ECCENTRICITY = 0.0167086;
+const EARTH_SEMI_MAJOR_AXIS = 149_598_023_000.0;
+
+//user only inputs mass ratio and eccentricity
+const params = createParams(
+	EARTH_ECCENTRICITY, MASS_SUN,
+	MASS_EARTH, EARTH_SEMI_MAJOR_AXIS
+);
+const rExact = [params.apoapsis, 0.0, 0.0];
+console.log(rExact);
+/*
+const [rEuler, tEuler] = test2Body(params, 1.0, MidpointEulerMutV);
+const errEuler = M3.Magnitude(rExact.Sub(rEuler));
+console.log("Euler time: "  + tEuler);
+console.log("Euler error: " + errEuler);
+console.log(rEuler);*/
+
+const [rRK4, tRK4] = test2Body(params, 4.0, RK4MutV);
+const errRK4 = M3.Magnitude(rExact.Sub(rRK4));
+console.log("RK4 time: "  + tRK4);
+console.log("RK4 error: " + errRK4);
+console.log(rRK4);
 
