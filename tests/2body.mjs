8003031941004a632039b7e844bca79b61b575e5
SabineWren <sabinewrensc@gmail.com>
2019-09-18 00:53:52 -0700

    More test changes. Math still wrong

--- tests/2body.mjs
+++ tests/2body.mjs
@@ -15,12 +15,13 @@ import { RK4MutV, MidpointEulerMutV } from "../scripts/integrate.mjs";
 import { ComputePositionAfterTime } from "../scripts/nBody.mjs";
 import * as M3 from "../scripts/matrices3D.mjs";
 const G = 6.6743015 * Math.pow(10,-11);
+const secToDay = seconds => seconds / 3600.0 / 24.0;
 
 const test2Body = function(params, dt, integrator) {
 	let r2 = [params.apoapsis, 0.0, 0.0];
 	let v2 = [0.0, -1.0, 0.0].ScaleMut(params.vApoapsis);
-	let r1 = r2.Scale(params.comRatio);
-	let v1 = v2.Scale(params.comRatio);
+	let r1 = r2.Scale(-1.0 * params.comRatio);
+	let v1 = v2.Scale(-1.0 * params.comRatio);
 	const start = Date.now();
 	
 	[r2, v2, r1, v1] = ComputePositionAfterTime(
@@ -32,60 +33,61 @@ const test2Body = function(params, dt, integrator) {
 	return [r2, delta];
 };
 
-const createParams = function(eccentricity, mass1, mass2, semiMajorAxis) {
+const createParams = function(ecc, m1, m2, a) {
 	//https://en.wikipedia.org/wiki/Apsis
-	const apoapsis  = semiMajorAxis * (1.0 + eccentricity);
-	//const periapsis = semiMajorAxis * (1.0 - eccentricity);
+	const apoapsis  = a * (1.0 + ecc);
 
 	//https://en.wikipedia.org/wiki/Barycenter
-	const centreOfMassRatio = -1.0 * mass2 / (mass1 + mass2);
+	const centreOfMassRatio = m2 / (m1 + m2);
 
 	//https://en.wikipedia.org/wiki/Orbital_speed
-	const vApoapsis = Math.sqrt(G * mass1 * (1.0 - eccentricity) / apoapsis);
+	//vis-viva equation
+	const vApoapsis = Math.sqrt(G * (m1 + m2) * (1.0 - ecc) / apoapsis);
+	console.log(vApoapsis);
 	
 	//https://en.wikipedia.org/wiki/Gravitational_two-body_problem
-	//T^2 G(m1 + m2) = 4 Pi^2 a^3
-	const aCubed = Math.pow(semiMajorAxis, 3);
-	const denom = G * (mass1 + mass2);
-	const orbitalPeriod = 2.0 * Math.PI * Math.sqrt(aCubed / denom);
+	const denom = G * (m1 + m2);
+	const orbitalPeriod = 2.0 * Math.PI * Math.sqrt(a * a * a / denom);
 	
-	console.log(orbitalPeriod / (3600.0 * 24.0));
 	return {
 		apoapsis: apoapsis,
 		comRatio: centreOfMassRatio,
-		m1: mass1,
-		m2: mass2,
+		m1: m1,
+		m2: m2,
 		orbitalPeriod: orbitalPeriod,
-		//orbitalPeriod: 365.25335 * 24.0 * 3600.0,
-		//orbitalPeriod: 365.256363004 * 24.0 * 3600.0,
-		semiMajorAxis: semiMajorAxis,
+		semiMajorAxis: a,
 		vApoapsis: vApoapsis,
 	};
 };
 
-//const secToDay = seconds => seconds / 3600.0 / 24.0;
-const MASS_SUN   = 1.9885    * Math.pow(10, 30);
-const MASS_EARTH = 5.97237   * Math.pow(10, 24);
-const EARTH_ECCENTRICITY = 0.0167086;
-const EARTH_SEMI_MAJOR_AXIS = 149_598_023_000.0;
+const printError = function(err, exact) {
+	console.log(err[0].toFixed() + ", " + (err[0] / exact[0] * 100.0).toFixed() + "%");
+	console.log(err[1].toFixed() + ", " + (err[1] / exact[1] * 100.0).toFixed() + "%");
+	console.log(err[2].toFixed() + ", " + (err[2] / exact[2] * 100.0).toFixed() + "%");
+};
 
-//user only inputs mass ratio and eccentricity
-const params = createParams(
-	EARTH_ECCENTRICITY, MASS_SUN,
-	MASS_EARTH, EARTH_SEMI_MAJOR_AXIS
-);
-const rExact = [params.apoapsis, 0.0, 0.0];
-console.log(rExact);
-/*
-const [rEuler, tEuler] = test2Body(params, 1.0, MidpointEulerMutV);
-const errEuler = M3.Magnitude(rExact.Sub(rEuler));
-console.log("Euler time: "  + tEuler);
-console.log("Euler error: " + errEuler);
-console.log(rEuler);*/
+const run2BodyTest = function(ecc, m1, m2, a) {
+	const params = createParams(ecc, m1, m2, a);
+	const rExact = [params.apoapsis, 0.0, 0.0];
+	console.log(rExact);
 
-const [rRK4, tRK4] = test2Body(params, 4.0, RK4MutV);
-const errRK4 = M3.Magnitude(rExact.Sub(rRK4));
-console.log("RK4 time: "  + tRK4);
-console.log("RK4 error: " + errRK4);
-console.log(rRK4);
+	const [rRK4, tRK4] = test2Body(params, 4.0, RK4MutV);
+	const err = rExact.Sub(rRK4);
+	console.log("RK4 time: "  + tRK4);
+	printError(err, rExact);
+	/*
+	const [rEuler, tEuler] = test2Body(params, 1.0, MidpointEulerMutV);
+	const errEuler = M3.Magnitude(rExact.Sub(rEuler));
+	console.log("Euler time: "  + tEuler);
+	console.log("Euler error: " + errEuler);
+	console.log(rEuler);*/
+}
+
+//Earth: 0.0167086
+//5.97237 * Math.pow(10, 24)
+//1.9885 * Math.pow(10, 30);
+//149_598_023_000.0;
+
+const massStar = 1.5 * Math.pow(10, 30);
+run2BodyTest(0.0, massStar, massStar, 50_000_000_000.0);
 
